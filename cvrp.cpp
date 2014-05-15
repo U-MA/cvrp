a98bd25fe91a30f737a3eb33a891c26389bd46ff
U-MA <g.0224incident@gmail.com>
2014-05-15 23:36:23 +0900

    demandとdepotを読み取れるようにした

--- cvrp.cpp
+++ cvrp.cpp
@@ -5,6 +5,11 @@
 #include "cvrp.h"
 
 namespace VrpSolver {
+
+    inline unsigned int Cvrp::demand(unsigned int node_id) {
+        return demands_[node_id];
+    }
+
     // 文字列strからtrim_char文字列に含まれている文字を削除
     void trim(std::string& str, const std::string& trim_char) {
         size_t pos;
@@ -20,12 +25,24 @@ namespace VrpSolver {
         return param;
     }
 
+    // 文字列を整数に変換する
+    int stoi(std::string str) {
+        std::istringstream iss(str);
+        int i;
+        iss >> i;
+        return i;
+    }
+
     // infileから情報を読み取りCvrpクラスをセットアップする
     void read_vrp(Cvrp& cvrp, const std::string &infile) {
         std::ifstream ifs(infile.c_str());
         if (!ifs) {
-            throw std::runtime_error("can't open file "+infile);
+            throw std::runtime_error("can't open file " + infile);
         }
+
+        std::string edge_weight_type,
+                    edge_weight_format,
+                    display_data_type;
 
         while (!ifs.eof()) {
             std::string tag;
@@ -35,12 +52,35 @@ namespace VrpSolver {
                 cvrp.name_ = get_parameter(ifs);
             }
             else if (tag == "DIMENSION") {
-                std::istringstream iss(get_parameter(ifs));
-                iss >> cvrp.dimension_;
+                cvrp.dimension_ = VrpSolver::stoi(get_parameter(ifs));
             }
             else if (tag == "CAPACITY") {
-                std::istringstream iss (get_parameter(ifs));
-                iss >> cvrp.capacity_;
+                cvrp.capacity_ = VrpSolver::stoi(get_parameter(ifs));
+            }
+            else if (tag == "DEMAND_SECTION") {
+                for (int i=0; i < cvrp.dimension_; i++) {
+                    unsigned int node_id, demand;
+                    ifs >> node_id >> demand;
+                    if (node_id != i+1)
+                        throw std::runtime_error("error:"
+                              "DEMAND_SECTION format may be different");
+                    cvrp.demands_.push_back(demand);
+                }
+            }
+            else if (tag == "EDGE_WEIGHT_TYPE") {
+                edge_weight_type = get_parameter(ifs);
+            }
+            else if (tag == "EDGE_WEIGHT_FORMAT") {
+                edge_weight_format = get_parameter(ifs);
+            }
+            else if (tag == "DISPLAY_DATA_TYPE") {
+                display_data_type = get_parameter(ifs);
+            }
+            else if (tag == "DEPOT_SECTION") {
+                cvrp.depot_ = VrpSolver::stoi(get_parameter(ifs));
+                if (VrpSolver::stoi(get_parameter(ifs)) != -1)
+                    throw std::runtime_error("error:"
+                          "can't handle multiple depots");
             }
             // 車体数を読み込むのは最後
         }
--- cvrp.h
+++ cvrp.h
@@ -1,14 +1,20 @@
 #include <string>
+#include <vector>
 
 namespace VrpSolver {
     class Cvrp {
     public:
-        Cvrp() : name_() {}
+        Cvrp() : name_(), dimension_(0), capacity_(0), demands_(0) {}
+
+        // accessor
+        unsigned int demand(unsigned int node_id);
 
         // member variable
         std::string name_;
         unsigned int dimension_;
         unsigned int capacity_;
+        unsigned int depot_;
+        std::vector<unsigned int> demands_;
     };
 
     // cvrp intialize by infile
