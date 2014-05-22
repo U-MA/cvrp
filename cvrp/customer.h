9f583c6971158fe3cc90fde18e3fb4bf9db1e5ae
U-MA <g.0224incident@gmail.com>
2014-05-22 12:48:16 +0900

    Customerクラスを作成

---
+++ cvrp/customer.h
@@ -0,0 +1,26 @@
+#ifndef VRPSOLVER_CUSTOMER_H
+#define VRPSOLVER_CUSTOMER_H
+
+namespace VrpSolver {
+
+    class Customer {
+    public:
+        Customer(unsigned int id, unsigned int demand) :
+            id_(id), demand_(demand) {}
+
+        unsigned int id() const {
+            return id_;
+        }
+
+        unsigned int demand() const {
+            return demand_;
+        }
+
+    private:
+        unsigned int id_;
+        unsigned int demand_;
+    };
+
+} // namespace VrpSolver
+
+#endif // VRPSOLVER_CUSTOMER_H
