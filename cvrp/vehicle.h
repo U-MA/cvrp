819bd62a7c471c87127c5cf334b357f5dac23776
U-MA <g.0224incident@gmail.com>
2014-05-23 09:12:55 +0900

    移動距離を返す関数を追加

--- cvrp/vehicle.h
+++ cvrp/vehicle.h
@@ -5,28 +5,36 @@
 #include <vector>
 
 #include <cvrp/customer.h>
+#include <cvrp/cvrp.h> // for Distance type
 
 namespace VrpSolver {
 
     class Vehicle {
     public:
-        Vehicle(unsigned int max_capacity) :
-            route_(), last_(Customer(1,0)), capacity_(0), max_capacity_(max_capacity) {}
+        Vehicle(unsigned int max_capacity,
+                const Distance *distance) :
+            route_(), current_(Customer(1,0)), mileage_(0),
+            capacity_(0), max_capacity_(max_capacity), distance_(distance) {}
 
         unsigned int capacity() const {
             return capacity_;
         }
 
+        // 走行距離を返す
+        unsigned int mileage() const;
+
         // 顧客cを訪問する
-        // その際、cの需要の分だけcapacityも増える
         void visit(const Customer& c);
 
     private:
-        Vehicle() {} // CAN NOT USE
+        int distance(const Customer &from, const Customer &to) const;
 
         std::vector<Customer> route_;
+        Customer current_;          // 最後に訪問した顧客
+        unsigned int mileage_;
         unsigned int capacity_;
         unsigned int max_capacity_; // 最大容量
+        const Distance *distance_;
 
         // Vehicleが保持しているroute_を出力する
         friend std::ostream& operator<<(std::ostream& ost, const Vehicle& v);
