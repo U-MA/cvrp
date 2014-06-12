ddb7155b9bdce3786630fd4dfe3118b8dbafbeab
U-MA <g.0224incident@gmail.com>
2014-06-13 00:10:01 +0900

    implementation VrpSolver::Fleet::is_feasible(size_t)

--- cvrp/fleet.cpp
+++ cvrp/fleet.cpp
@@ -10,4 +10,40 @@ namespace VrpSolver {
         return sum_distance;
     }
 
+    int Vehicle::distance(const Customer &c1, const Customer &c2) const {
+        const int from = c1.id();
+        const int to   = c2.id();
+        const int index = (to > from) ? ((to-2)*(to-1)/2+(from-1)) :
+                                        ((from-2)*(from-1)/2+(to-1));
+        return distance_[index];
+    }
+
+    unsigned int Vehicle::mileage() const {
+        Customer depot(1, 0);
+        return mileage_ + distance(current_, depot);
+    }
+
+    void Vehicle::visit(const Customer& c) {
+        if (!can_visit(c))
+            throw std::runtime_error("vehicle overloaded");
+        capacity_ += c.demand();
+        mileage_  += distance(current_, c);
+        route_.push_back(c);
+        current_ = c;
+        if (fleet_ != nullptr)
+            fleet_->is_visit_.set(c.id());
+    }
+
+    bool Vehicle::can_visit(const Customer& c) const {
+        return capacity_ + c.demand() <= max_capacity_;
+    }
+
+    std::ostream& operator<<(std::ostream& ost, const Vehicle& v) {
+        auto route = v.route_;
+        for (int i=0; i < static_cast<int>(route.size()); i++)
+            ost << route[i].id() << " ";
+        return ost;
+    }
+
+
 } // namespace VrpSolver
--- cvrp/fleet.h
+++ cvrp/fleet.h
@@ -1,13 +1,53 @@
 #ifndef VRPSOLVER_FLEET_H
 #define VRPSOLVER_FLEET_H
 
+#include <bitset>
 #include <iterator>
+#include <stdexcept>
 #include <vector>
 
 #include <cvrp/cvrp.h>
-#include <cvrp/vehicle.h>
 
 namespace VrpSolver {
+
+    class Fleet;
+
+    class Vehicle {
+    public:
+        Vehicle(unsigned int max_capacity,
+                const DistanceList &distance,
+                Fleet* fleet = nullptr) :
+            route_(), current_(Customer(1,0)), mileage_(0),
+            capacity_(0), max_capacity_(max_capacity), distance_(distance), fleet_(fleet) {}
+
+        unsigned int capacity() const {
+            return capacity_;
+        }
+
+        // 走行距離を返す
+        unsigned int mileage() const;
+
+        // 顧客cを訪問する
+        void visit(const Customer& c);
+
+        // 顧客cを訪問可能か
+        bool can_visit(const Customer& c) const;
+
+    private:
+        int distance(const Customer &from, const Customer &to) const;
+
+        std::vector<Customer> route_;
+        Customer current_;          // 最後に訪問した顧客
+        unsigned int mileage_;
+        unsigned int capacity_;
+        unsigned int max_capacity_; // 最大容量
+        const DistanceList &distance_;
+        Fleet* fleet_; // 所属している車団
+
+        // Vehicleが保持しているroute_を出力する
+        friend std::ostream& operator<<(std::ostream& ost, const Vehicle& v);
+    };
+
 
     class Fleet {
     public:
@@ -17,7 +57,8 @@ namespace VrpSolver {
               distance_list_(distance_list), fleet_()
         {
             for (size_t i=0; i < size; i++)
-                fleet_.push_back(Vehicle(max_capacity, distance_list));
+                fleet_.push_back(Vehicle(max_capacity, distance_list, this));
+            is_visit_.set(0);
         }
 
         unsigned int distance() const;
@@ -30,6 +71,14 @@ namespace VrpSolver {
 
         std::vector<Vehicle>::iterator end()
         { return fleet_.end(); }
+
+        bool is_visit(size_t id)
+        { return is_visit_.test(id); }
+
+        bool is_feasible(size_t dimension)
+        { return is_visit_.count() == dimension; }
+
+        std::bitset<101> is_visit_;
 
     private:
         size_t size_;
