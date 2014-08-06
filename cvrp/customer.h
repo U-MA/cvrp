6ee57a1a54fda1564a2443b7606aeb0b40af6485
U-MA <g.0224incident@gmail.com>
2014-08-06 23:36:25 +0900

    Add to member vertex, x(), y() and some constractor
    ビューアのために座標を格納出来るようにした.
    それに加えて必要かわからないが２つのコンストラクタを実装した

--- cvrp/customer.h
+++ cvrp/customer.h
@@ -1,12 +1,22 @@
 #ifndef VRPSOLVER_CUSTOMER_H
 #define VRPSOLVER_CUSTOMER_H
 
+#include <utility>
+
 namespace VrpSolver {
 
     class Customer {
     public:
         Customer(std::size_t id, std::size_t demand)
-            : id_(id), demand_(demand)
+            : id_(id), demand_(demand), vertex_(std::make_pair(0, 0))
+        { }
+
+        Customer(std::size_t id, std::size_t demand, std::pair<int, int> vertex)
+            : id_(id), demand_(demand), vertex_(vertex)
+        { }
+
+        Customer(std::size_t id, std::size_t demand, int x, int y)
+            : id_(id), demand_(demand), vertex_(std::make_pair(x, y))
         { }
 
         std::size_t id() const {
@@ -17,9 +27,18 @@ namespace VrpSolver {
             return demand_;
         }
 
+        int x() const {
+            return vertex_.first;
+        }
+
+        int y() const {
+            return vertex_.second;
+        }
+
     private:
         std::size_t id_;
         std::size_t demand_;
+        std::pair<int, int> vertex_;
     };
 
 } // namespace VrpSolver
