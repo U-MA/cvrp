83813ac1b36a10d1e094aff1b2ab66495a3cdcbd
U-MA <g.0224incident@gmail.com>
2014-08-11 09:34:35 +0900

    Move tsplib keywords to file tsplib_keyword.h
    tsplibのキーワードはcvrp.cpp
    に無くてよいと判断した

--- cvrp/cvrp.cpp
+++ cvrp/cvrp.cpp
@@ -24,59 +24,7 @@ namespace VrpSolver {
         return param;
     }
 
-
     typedef std::string keyword;
-
-    enum TsplibKeyword {
-        // The specification part
-        NAME, TYPE, COMMENT, DIMENSION, CAPACITY,
-        EDGE_WEIGHT_TYPE, EDGE_WEIGHT_FORMAT, EDGE_DATA_FORMAT,
-        NODE_COORD_TYPE, DISPLAY_DATA_TYPE, END_OF_FILE,
-
-        // The data part
-        NODE_COORD_SECTION, DEPOT_SECTION, DEMAND_SECTION,
-        EDGE_DATA_SECTION, EDGE_WEIGHT_SECTION,
-    };
-
-    std::map<keyword, TsplibKeyword> keyword_map = {
-        // The specification part
-        { "NAME",                NAME },
-        { "TYPE",                TYPE },
-        { "COMMENT",             COMMENT },
-        { "DIMENSION",           DIMENSION },
-        { "CAPACITY",            CAPACITY },
-        { "EDGE_WEIGHT_TYPE",    EDGE_WEIGHT_TYPE },
-        { "EDGE_WEIGHT_FORMAT",  EDGE_WEIGHT_FORMAT },
-        { "EDGE_DATA_FORMAT",    EDGE_DATA_FORMAT },
-        { "NODE_COORD_TYPE",     NODE_COORD_TYPE },
-        { "DISPLAY_DATA_TYPE",   DISPLAY_DATA_TYPE },
-        { "EOF",                 END_OF_FILE },
-
-        // The data part
-        { "NODE_COORD_SECTION",  NODE_COORD_SECTION },
-        { "DEPOT_SECTION",       DEPOT_SECTION },
-        { "DEMAND_SECTION",      DEMAND_SECTION },
-        { "EDGE_DATA_SECTION",   EDGE_DATA_SECTION },
-        { "EDGE_WEIGHT_SECTION", EDGE_WEIGHT_SECTION }
-    };
-
-    enum EdgeWeightType {
-        EXPLICIT, EUC_2D
-    };
-
-    std::map<keyword, EdgeWeightType> ew_type_map = {
-        { "EXPLICIT", EXPLICIT },
-        { "EUC_2D",   EUC_2D }
-    };
-
-    enum EdgeWeightFormat {
-        LOWER_ROW
-    };
-
-    std::map<keyword, EdgeWeightFormat> ew_format_map = {
-        { "LOWER_ROW", LOWER_ROW }
-    };
-
 
     void Cvrp::read_vrp(const std::string &infile) {
         std::ifstream ifs(infile.c_str());
